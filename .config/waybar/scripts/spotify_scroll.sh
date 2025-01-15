df31fa032ef06d671caa372146e1e25cfa645bba
Turja Roy <turja.roy@outlook.com>
2025-01-15 06:22:12 -0600

    waybar module name

--- .config/waybar/scripts/spotify_scroll.sh
+++ .config/waybar/scripts/spotify_scroll.sh
@@ -2,9 +2,9 @@
 zscroll -l 20 \
     --delay 0.1 \
     --scroll-padding "     " \
-    --match-command "playerctl status" \
+    --match-command "playerctl --player=spotify status" \
     --match-text "Playing" "--scroll 1" \
     --match-text "Paused" "--scroll 0" \
     --update-check true \
-    "playerctl metadata --format '{{ artist }} - {{ title }}'"
+    "playerctl --player=spotify metadata --format '{{ artist }} - {{ title }}'"
 wait

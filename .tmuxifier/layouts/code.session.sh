ed2789bf1fdf17bbb0da186aac3a1ad39679c7da
Turja Roy <turja.roy@outlook.com>
2025-02-19 15:21:48 -0600

    new tmuxifier session for coding session

---
+++ .tmuxifier/layouts/code.session.sh
@@ -0,0 +1,23 @@
+session_root "$(pwd)"
+
+if initialize_session "coding"; then
+
+	new_window "Coding"
+	if [[ -d "$(pwd)/src" ]]; then
+		run_cmd "cd $(pwd)/src"
+	else
+		run_cmd "cd $(pwd)"
+	fi
+	run_cmd "clear"
+	run_cmd "nv"
+
+	new_window "Nav"
+	run_cmd "cd $(pwd)"
+	run_cmd "clear"
+	run_cmd "ranger"
+
+	select_window 1
+
+fi
+
+finalize_and_go_to_session

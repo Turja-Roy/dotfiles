0c07bfac6eaed034056c29eb14bc7cff66190297
Turja Roy <turja.roy@outlook.com>
2025-09-10 15:00:07 -0500

    Update 2025-09-10 14:59

---
+++ .tmuxifier/layouts/cter.session.sh
@@ -0,0 +1,22 @@
+session_root "$(pwd)"
+
+if initialize_session $(basename $(pwd)); then
+
+	new_window "Coding"
+	if [[ -d "$(pwd)/src" ]]; then
+		run_cmd "cd $(pwd)/src"
+	else
+		run_cmd "cd $(pwd)"
+	fi
+	run_cmd "clear"
+	run_cmd "nv"
+
+	new_window "Terminal"
+	run_cmd "cd $(pwd)"
+	run_cmd "clear"
+
+	select_window 1
+
+fi
+
+finalize_and_go_to_session
